27a97f1703efebcee72b47612d1b7e39bbe5c04a
LeHuman <47549331+LeHuman@users.noreply.github.com>
2020-04-20 14:43:19 -0500

    oof this was easier than what i thought

--- Client.py
+++ Client.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python3
-
 import json
 import socket
 import ssl
@@ -7,63 +5,71 @@ import urllib.request
 from enum import Enum
 import time
 from threading import Thread
+import concurrent.futures
+import os
 
 HOST = "www.koolkidz.club"
 PORT = 4578
 TARGET_SERVER = "mc.koolkidz.club"
 API_URL = "https://api.mcsrvstat.us/2/" + TARGET_SERVER
 context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+DEBUG = False
 
 
 class Command(Enum):
-    triggerOn = "15".encode()
-    triggerOff = "12".encode()  # is this a good idea?
-    check = "8".encode()
+    turnOn = "15".encode()
+    turnOff = "12".encode()  # is this a good idea?
+    status = "8".encode()
+
+
+def log(*args):
+    if DEBUG:
+        for count, thing in enumerate(args):
+            print("{0}. {1}".format(count, thing))
 
 
 def getAltStatus():
     with urllib.request.urlopen(API_URL) as url:
         data = json.loads(url.read().decode())
-        print(data["hostname"], "is online" if data["online"] else "is offline")
-        if data["motd"]:
-            print(data["motd"]["clean"])
-        if data["players"]:
-            print("Players online:", data["players"]["online"])
+        final = (
+            TARGET_SERVER
+            + " is currently "
+            + ("online" if data["online"] else "offline")
+        )
+        # if "motd" in data:
+        #     final += " " + data["motd"]["clean"]
+        if "players" in data:
+            final += " Players online: " + data["players"]["online"]
+        return final
 
 
-def query(cmd):
+def _newQuery(cmd):
     try:
         with socket.create_connection((HOST, PORT)) as sock:
             with context.wrap_socket(sock, server_hostname=HOST) as ssock:
-                ssock.settimeout(2)
+                ssock.settimeout(5)
                 ssock.send(cmd.value)
-                print(ssock.recv(256).decode())
+                msg = ssock.recv(256).decode()
                 ssock.close()
-                return 1
+                return msg
     except ssl.SSLError as e:
-        print(e)
+        log(e)
     except ConnectionResetError as e:
-        print(e)
+        log(e)
+    except Exception as e:
+        log(e)
+        return "There has been an error connecting to the server!"
 
-class spam2(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self.start()
-    def run(self):
-        spam()
-        for _ in range(20):
-            query(Command.triggerOff)
-            query(Command.triggerOn)
 
-class spam(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self.start()
-    def run(self):
-        spam2()
-        for _ in range(20):
-            query(Command.triggerOff)
-            query(Command.triggerOn)
+def newThread(trgt):
+    thread = Thread(target=trgt)
+    thread.daemon = True
+    thread.start()
+    return thread
 
-spam()
-# query(Command.triggerOff)
+
+def query(cmd):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(_newQuery, cmd)
+        result = future.result()
+        return result
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Discord Server Controller
-  Using a pi0, that can be controlled by a discord bot, it should be able to use its GPIO pins to physically turn on a bigger server
+# PythonSocketTest
+Nvm just still testing this python doohickey
